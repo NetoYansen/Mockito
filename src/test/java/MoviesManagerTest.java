6ac349a7118e00a4c6fe25d8d819d4503397f971
User <yansenlv@mail.ru>
2023-05-31 22:50:11 +0300

    Initial commit

---
+++ src/test/java/MoviesManagerTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoviesManagerTest {
+
+    @Test
+    public void testAdd0() {
+        MoviesManager manager = new MoviesManager();
+
+        String[] expected = {};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testAdd1() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.addMovie("Film 1");
+
+        String[] expected = {"Film 1"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testAdd3() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testLast3() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testLast6() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+
+        String[] expected = {"Film 6", "Film 5", "Film 4", "Film 3", "Film 2"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
